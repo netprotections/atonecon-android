de5e28e761e4e2619c3f33ca29d8422ee6e08a55
at-hoaiphan <hoai.phan@asiantech.vn>
2017-08-04 17:45:59 +0700

    Check network

--- atonelibrary/src/main/java/atone/asiantech/vn/atonelibrary/AtonePay.java
+++ atonelibrary/src/main/java/atone/asiantech/vn/atonelibrary/AtonePay.java
@@ -1,10 +1,17 @@
 package atone.asiantech.vn.atonelibrary;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 import atone.asiantech.vn.atonelibrary.models.Payment;
 
@@ -37,18 +44,29 @@ public class AtonePay {
     }
 
     public void performPayment(Activity context, Payment payment) {
-        context.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        JavaScriptInterface javaScriptInterface = new JavaScriptInterface(payment, mOption);
-        javaScriptInterface.setCallBackHandler(mOnTransactionCallBack);
-
-        FragmentTransaction fragmentTransaction = context.getFragmentManager().beginTransaction();
-        Fragment fragment = context.getFragmentManager().findFragmentByTag("fragment");
-        if (fragment != null) {
-            fragmentTransaction.remove(fragment);
+        if (ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "Please check for Access Network State in your app!", Toast.LENGTH_SHORT).show();
+            return;
         }
-        fragmentTransaction.addToBackStack(null);
-        mDialogFragment = WebViewDialogFragment.getInstance(javaScriptInterface);
-        mDialogFragment.show(fragmentTransaction, "fragment");
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            context.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            JavaScriptInterface javaScriptInterface = new JavaScriptInterface(payment, mOption);
+            javaScriptInterface.setCallBackHandler(mOnTransactionCallBack);
+
+            FragmentTransaction fragmentTransaction = context.getFragmentManager().beginTransaction();
+            Fragment fragment = context.getFragmentManager().findFragmentByTag("fragment");
+            if (fragment != null) {
+                fragmentTransaction.remove(fragment);
+            }
+            fragmentTransaction.addToBackStack(null);
+            mDialogFragment = WebViewDialogFragment.getInstance(javaScriptInterface);
+            mDialogFragment.show(fragmentTransaction, "fragment");
+        } else {
+            Toast.makeText(context, "Please check network connection!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void resetToken() {
