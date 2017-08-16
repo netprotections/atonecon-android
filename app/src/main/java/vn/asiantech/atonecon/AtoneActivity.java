9977fbe31789d29d8acc7ccd2fa83cc51f6e891e
at-hoaiphan <hoai.phan@asiantech.vn>
2017-08-16 10:52:05 +0700

    Fix javadoc

--- app/src/main/java/vn/asiantech/atonecon/AtoneActivity.java
+++ app/src/main/java/vn/asiantech/atonecon/AtoneActivity.java
@@ -147,7 +147,7 @@ public class AtoneActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    /*
+    /**
      * Class runs in background to update Token when webView is showing
      */
     private class PreKeyAsyncTask extends AsyncTask<String, String, String> {
