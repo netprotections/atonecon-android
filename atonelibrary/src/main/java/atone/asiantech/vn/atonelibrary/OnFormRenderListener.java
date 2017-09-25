b687b680975ab7b7fece1a44ae229247945f29ed
at-hoaiphan <hoai.phan@asiantech.vn>
2017-09-25 17:40:06 +0700

    Remove unnecesary javadoc

--- atonelibrary/src/main/java/atone/asiantech/vn/atonelibrary/OnFormRenderListener.java
+++ atonelibrary/src/main/java/atone/asiantech/vn/atonelibrary/OnFormRenderListener.java
@@ -1,9 +1,5 @@
 package atone.asiantech.vn.atonelibrary;
 
-/**
- * An interface gets callback from JavaScriptInterface when form loading success in web-view.
- */
-
 public interface OnFormRenderListener {
     void onFormTransactionOpened();
 }
